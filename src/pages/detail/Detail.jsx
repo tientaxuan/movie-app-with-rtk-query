b022645ab79cfa35acbddc5cd0574d06d2317e4f
tientaxuan <tiendtypbn@gmail.com>
2022-04-13 16:18:38 +0700

    add useEffect to Detail.jsx

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
 import './detail.scss';
 
@@ -21,7 +21,9 @@ export const Detail = () => {
     cate: category,
     id: id,
   });
-
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  });
   return (
     <>
       {isLoading ? (
